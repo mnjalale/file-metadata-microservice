96c237083d9b1aa20dea666c903f762799081f6c
Martin Njalale <mnjalale@gmail.com>
2016-07-04 10:39:46 +0000

    uploading functionality added

--- app/controllers/client/indexController.js
+++ app/controllers/client/indexController.js
@@ -1,5 +1,12 @@
 'use strict';
 
 (function(){
+     //document.getElementById('fileForm').action = processFile;
     
+    this.processFile = function (){
+        ajaxFunctions.ajaxRequest('POST',appUrl + '/upload',function(data){
+            var fileInfo = JSON.parse(data);
+            alert('File Size:' + fileInfo.size);
+        });
+    };
 })();
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,24 @@
 'use strict';
 
 var path = process.cwd();
+var multer = require("multer");
+var upload = multer({ dest: './uploads/'});
+
 module.exports = function(app){
     
     app.route('/')
         .get(function(req,res){
             res.sendFile(path + '/public/index.html');
         });
+    
+    app.post('/upload',upload.single('file'),function(req,res,next){
+        console.log(req);
+        if(req.file){
+            var file = req.file;
+            res.json({ "size": file.size, "name": file.originalname, "type":file.mimetype});
+        }else{
+            res.json({ "error": "No file uploaded."});
+        }
+       
+    });
 };
